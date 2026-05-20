5fbd869da0e48e45194fc9f15883966fefc55796
Pranay Talware <pranaytalware7@gmail.com>
2026-05-20 23:57:29 +0530

    Scanner class Implementation

--- Day 03 - Input Output — Scanner Class/InoutOutput.java
+++ Day 03 - Input Output — Scanner Class/InoutOutput.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class InoutOutput {
 
+    @SuppressWarnings("ConvertToTryWithResources")
     public static void main(String[] args) {
 
         // Instantiate the scanner object linked to standard input
