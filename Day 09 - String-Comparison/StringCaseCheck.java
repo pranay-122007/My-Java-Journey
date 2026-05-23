f59cd31151c17a70e4241cbae5d5e4fdf4387148
Pranay Talware <pranaytalware7@gmail.com>
2026-05-23 09:59:01 -0700

    Delete Day 09 - String-Comparison directory

--- Day 09 - String-Comparison/StringCaseCheck.java
+++
@@ -1,9 +0,0 @@
-public class StringCaseCheck {
-    public static void main(String[] args) {
-        String original = "JAVA";
-        String lower = "java";
-
-        System.out.println("Regular Equals: " + original.equals(lower)); // false
-        System.out.println("Ignore Case Equals: " + original.equalsIgnoreCase(lower)); // true
-    }
-}
